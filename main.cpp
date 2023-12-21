15170a3c10826979d0181bede22b5fd889283758
ipek-debreli <ipek.debreli@ozu.edu.tr>
2023-12-21 13:23:16 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by id033803 on 12/21/2023.
+//
+#include <iostream>
+#include "src/List/Graph/Graph.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "test" << endl;
+
+    list::Graph gr{1};
+}
